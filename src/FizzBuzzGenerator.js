2be2494e8211b6553b34e27ff377f221827fec57
Muhammed Haque <nohorjo@gmail.com>
2019-09-05 17:31:11 +0200

    #new - Add custom terms

--- src/FizzBuzzGenerator.js
+++ src/FizzBuzzGenerator.js
@@ -1,20 +1,24 @@
 export default function({
     limit = 100,
+    terms = [
+        ['Fizz', 3],
+        ['Buzz', 5],
+    ],
 } = {}) {
     const results = [];
 
     for (let i = 1; i <= limit; i++) {
-        if (!(i % 15)) {
-            results.push('FizzBuzz');
-        } else {
-            if (!(i % 3)) {
-                results.push('Fizz');
-            } else if (!(i % 5)) {
-                results.push('Buzz');
-            } else {
-                results.push(String(i));
-            }
-        }
+        let string = '';
+        
+        terms.forEach(([term, modder]) => {
+            if (!(i % modder))
+                string += term;
+        });
+
+        if (!string)
+            string = String(i);
+
+        results.push(string);
     }
 
     return results;
